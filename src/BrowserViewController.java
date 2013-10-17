04a835f6c2368a572c38adafa710e68884d245ca
kmzn <kmzn777@gmail.com>
2013-10-18 00:19:49 +0900

    get rid of dead code

--- src/BrowserViewController.java
+++ src/BrowserViewController.java
@@ -1,11 +1,6 @@
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,31 +11,21 @@ import java.util.logging.Logger;
 import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Worker;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
 import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebEvent;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
 
 public class BrowserViewController implements Initializable {
     @FXML
@@ -51,8 +36,6 @@ public class BrowserViewController implements Initializable {
     private WebView webView;
     @FXML
     private TextArea editArea;
-    @FXML
-    private Button button;
     private FileChooser fileChooser = new FileChooser();
     private WebEngine webEngine;
     private WebHistory webHistory;
@@ -62,7 +45,6 @@ public class BrowserViewController implements Initializable {
     
     private Boolean isFileLoading = false;
     private static String OutputFileName = "temp.md";
-    private File outputFile = new File(OutputFileName);
     private String mdFilePath;
     
     static String PANDOC = "pandoc -s -f markdown -t html5 --highlight-style=tango ";
@@ -105,37 +87,6 @@ public class BrowserViewController implements Initializable {
         if (webHistory.getCurrentIndex() < webHistory.getEntries().size()) {
             webHistory.go(1);
         }
-    }
-    
-    private void showDialog(String message) {
-        final Stage dialog = new Stage();
-        dialog.initModality(Modality.APPLICATION_MODAL);
-        dialog.setTitle("Alert");
-        dialog.setResizable(false);      
-
-        BorderPane root = new BorderPane();
-        root.setPadding(new Insets(10));
-
-        Label messageLabel = new Label(message);
-        messageLabel.setFont(Font.font(null, 16.0));
-        root.setCenter(messageLabel);
-        BorderPane.setAlignment(messageLabel, Pos.CENTER);
-        
-        Button closeButton = new Button("OK");
-        closeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent t) {
-                dialog.close();
-            }
-        });
-        
-        root.setBottom(closeButton);
-        BorderPane.setAlignment(closeButton, Pos.CENTER);
-        
-        Scene scene = new Scene(root);
-        dialog.setScene(scene);
-        
-        dialog.show();
     }
 
     @Override
@@ -210,27 +161,6 @@ public class BrowserViewController implements Initializable {
         // ヒストリを取得
         webHistory = webEngine.getHistory();
         
-        // JavaScriptのalertがコールされた場合のイベント処理
-        webEngine.setOnAlert(new EventHandler<WebEvent<String>>() {
-            @Override
-            public void handle(WebEvent<String> event) {
-               showDialog(event.getData());
-            }
-        });
         
-        // ページのロードの状態に応じた処理
-        Worker<Void> worker = webEngine.getLoadWorker();
-        worker.stateProperty().addListener(new ChangeListener<Worker.State>() {
-            @Override
-            public void changed(ObservableValue<? extends Worker.State> value, 
-                                 Worker.State oldState, Worker.State newState) {
-                if (newState == Worker.State.SUCCEEDED) {
-                    // ページのロードに成功したらURLをテキストフィールドに表示する
-                    urlField.setText(webEngine.getLocation());
-                } else if (newState == Worker.State.FAILED) {
-                    
-                }
-            }
-        });
     }
 }
